a818adf1c0cfdd22d1536c7484543a3411abf1a9
phuochieu <phieeus99@gmail.com>
2024-07-06 20:32:51 +0700

    Api Get Details Board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,10 +1,12 @@
 /* eslint-disable quotes */
 /* eslint-disable no-useless-catch */
+import { StatusCodes } from "http-status-codes"
 import { boardModel } from "~/models/boardModel"
+import ApiError from "~/utils/ApiError"
 import { slugify } from "~/utils/formatters"
 const createNew = async (reqBody) => {
   try {
-    // Xử lý logic dữ liệu tùy đặc thù dự án
+    // Xử lý logic dữ liệu từ đặc thù dự án
     const newBoard = {
       ...reqBody,
       slug: slugify(reqBody.title)
@@ -22,6 +24,18 @@ const createNew = async (reqBody) => {
     return getNewBoard
   } catch (error) {throw error}
 }
+const getDetails = async (boardId) => {
+  try {
+
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found')
+    }
+
+    return board
+  } catch (error) {throw error}
+}
 export const boardService = {
-  createNew
+  createNew,
+  getDetails
 }
